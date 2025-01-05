a6bd5f4e0dac14ec01f79972999a5d5164f1a91e
Maria Rios Navarro <propscode254@gmail.com>
2025-01-05 16:34:20 +0100

    finish earth

--- index-earth.js
+++ index-earth.js
@@ -1,7 +1,8 @@
 import * as THREE from "three";
 import {OrbitControls} from "jsm/controls/OrbitControls.js";
 
-
+import getStartfield from "./src/getStarfield.js";
+import {getFresnelMat} from "./src/getFresnelMat.js";
 
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -14,41 +15,82 @@ renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
 
 
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = 23.4 * Math.PI / 180;
+scene.add(earthGroup);
+
 
 // Controls
 
-new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-// const controls = new OrbitControls(camera, renderer.domElement);
-// controls.enableDamping = true;
-// controls.dampingFactor = 0.25;
 
 
 // 3d-Objects in the scene
 
+const detail = 12;
 
-const geometry = new THREE.IcosahedronGeometry(1, 24);
+const loader = new THREE.TextureLoader();
+const geometry = new THREE.IcosahedronGeometry(1, detail);
 const material = new THREE.MeshStandardMaterial({
-    color: 0xffff00,
-    // flatShading: true
+    map: loader.load('/textures/00_earthmap1k.jpg')
 });
+
+// Earth
 const earthMesh = new THREE.Mesh(geometry, material);
-scene.add(earthMesh);
+earthGroup.add(earthMesh);
+
+// Earth Light
+
+const lightMat = new THREE.MeshBasicMaterial({
+    map:loader.load("/textures/03_earthlights1k.jpg"),
+    blending: THREE.AdditiveBlending,
+});
+const lightMesh = new THREE.Mesh(geometry, lightMat);
+
+earthGroup.add(lightMesh);
 
 
+//Glow
+
+const glowMat = getFresnelMat();
+const glowMesh = new THREE.Mesh(geometry, glowMat);
+glowMesh.scale.setScalar(1.01);
+earthGroup.add(glowMesh);
 
 
+// Earth Clouds
 
-// Add 3d effect to the object
+const cloudsMat = new THREE.MeshBasicMaterial({
+    map:loader.load("/textures/04_earthcloudmap.jpg"),
+    transparent: true,
+    opacity: 0.2,
+    blending: THREE.AdditiveBlending,
+});
 
-const hemilight = new THREE.HemisphereLight(0x000000, 0xFFFFFF);
-scene.add(hemilight);
+const cloudMesh = new THREE.Mesh(geometry, cloudsMat);
+cloudMesh.scale.setScalar(1.02);
+earthGroup.add(cloudMesh);
 
+
+//Stars
+
+const stars = getStartfield(2000);
+scene.add(stars);
+
+
+// SUNLIGHT
+
+const sunLight = new THREE.DirectionalLight(0xffffff, 1);
+sunLight.position.set(-2, -0.5, 1.5);
+scene.add(sunLight);
 
 function animate() {
     requestAnimationFrame(animate);
-    earthMesh.rotation.x += 0.001;
     earthMesh.rotation.y += 0.002;
+    lightMesh.rotation.y += 0.002;
+    cloudMesh.rotation.y += 0.0023;
+    glowMesh.rotation.y += 0.002;
     renderer.render(scene, camera);
     controls.update();
 }
@@ -69,36 +111,3 @@ animate();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const scene = new THREE.Scene();
-
-
-// const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-// camera.position.z = 5;
-
-
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
-
-
-// function animate() {
-//     requestAnimationFrame(animate);
-//     cube.rotation.x += 0.01;
-//     cube.rotation.y += 0.01;
-//     renderer.render(scene, camera);
-// }
-
-// animate();
